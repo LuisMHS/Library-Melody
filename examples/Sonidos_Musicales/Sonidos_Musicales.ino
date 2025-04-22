3b2382747a6e372ea4378817faa9ffd0dc76fac3
Luis Miguel Hernández Samaniego <48294999+LuisMHS@users.noreply.github.com>
2025-04-22 16:19:02 -0500

    Add files via upload

---
+++ examples/Sonidos_Musicales/Sonidos_Musicales.ino
@@ -0,0 +1,47 @@
+/*
+   @Sketch: Sonidos_Musicales.ino
+   @author: Luis Miguel Hernández Samaniego
+   Arduino: 2.3.4
+   @version ESP32: 3.1.1
+    Arduino: 2.3.6
+   @version ESP32: 3.2
+   
+*/
+
+//-------------------------------------------------//
+//incluir el archivo Notas.h
+#include <Notas.h>
+//-------------------------------------------------//
+
+//-------------------------------------------------//
+//Creación de un Objeto
+Notas Melody(14);
+//-------------------------------------------------//
+
+void setup() {
+  //-------------------------------------------------//
+  //Inicializar la librería Notas
+  Melody.begin();
+  //-------------------------------------------------//
+
+  //-------------------------------------------------//
+  //Inicializar la comunicación con la PC
+  Serial.begin(9600);
+  //-------------------------------------------------//
+}
+void loop() {
+  //-------------------------------------------//
+  Melody.sound_Connection();
+  //-------------------------------------------//
+  //Happy Birthday to You
+  Melody.sound_Happy_Birthday();
+  //-------------------------------------------//
+  //Noche de paz
+  Melody.sound_Noche_paz();
+  //-------------------------------------------//
+  //Christmas music
+  Melody.sound_Christmas_Music();
+  //-------------------------------------------//
+  Melody.sound_Disconnection();
+  //-------------------------------------------//
+}
